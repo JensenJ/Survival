304de35e16d51769438d95ca09bf73e47a2d1205
JensenJ <jensenbradley@icloud.com>
2019-02-17 22:18:25 +0000

    Seasons added.
    Seasons added to environment controller.

--- Assets/Scripts/EnvironmentController.cs
+++ Assets/Scripts/EnvironmentController.cs
@@ -8,37 +8,35 @@ public class EnvironmentController : MonoBehaviour
     [Header("References: ")]
     [SerializeField] private Light sun;
 
-    //[Space(20)]
     [Header("Time: ")]
     [SerializeField] private bool bIsTimeEnabled = true;
     [SerializeField] private float timeMultiplier = 1f;
     [SerializeField] private int DaysInMonth = 30;
-    [SerializeField] private int MonthsInYear = 4;
+    [SerializeField] private int MonthsInYear = 12;
 
-    private float sunInitialIntensity;
     [Range(0, 1)] [SerializeField] private float currentTimeOfDay = 0;
 
     [Space(10)]
     [SerializeField] private int Seconds = 0;
     [SerializeField] private int Minutes = 0;
     [SerializeField] private int Hours = 0;
-
-    [Space(10)]
     [SerializeField] private int Day = 1;
     [SerializeField] private int Month = 1;
     [SerializeField] private int Year = 1;
-
     [SerializeField] private float Clockwork;
-    private float DayTick;
-    private float DayNightHours;
-    private int dayTracker = 1;
 
-    //[Space(20)]
+    enum ESeasonEnum { ENone, ESpring, ESummer, EAutumn, EWinter };
+    [SerializeField] ESeasonEnum SeasonEnum;
+
+    private float DayTick;
+    private float sunInitialIntensity;
+    private float DayNightHours;
+
     [Header("Temperature:")]
     [SerializeField] float tempMultiplier = 1;
-    [SerializeField] bool bNewGenerationTemp = true;
-    [SerializeField] bool bHasGeneratedTemp = false;
     [SerializeField] bool bIsTempFahrenheit = false;
+    bool bNewGenerationTemp = true;
+    bool bHasGeneratedTemp = false;
     float lastTemp = 0;
     float minGenTemp = 0;
     float maxGenTemp = 7;
@@ -54,20 +52,17 @@ public class EnvironmentController : MonoBehaviour
 
     void Update()
     {
-        if (bIsTimeEnabled)
+        if (!bIsTimeEnabled)
         {
-            SetClockwork();
-            Clock();
-            Calendar();
-            UpdateSun();
+            return;
         }
+        SetClockwork();
+        Clock();
+        Calendar();
 
-        UpdateEnvironment();
-    }
-
-    void UpdateEnvironment()
-    {
-        print(Temperature());
+        UpdateSun();
+        Temperature();
+        Season();
     }
 
     void SetClockwork()
@@ -141,6 +136,31 @@ public class EnvironmentController : MonoBehaviour
         }
     }
 
+    ESeasonEnum Season()
+    {
+        if (Month == 12 || Month == 1 || Month == 2)
+        {
+            return ESeasonEnum.EWinter;
+        }
+        else if (Month == 3 || Month == 4 || Month == 5)
+        {
+            return ESeasonEnum.ESpring;
+        }
+        else if (Month == 6 || Month == 7 || Month == 8)
+        {
+            return ESeasonEnum.ESummer;
+        }
+        else if (Month == 9 || Month == 10 || Month == 11)
+        {
+            return ESeasonEnum.EAutumn;
+        }
+        else
+        {
+            print("Season::Setting season failed!");
+            return ESeasonEnum.ENone;
+        }
+    }
+
     float Temperature()
     {
         if (bNewGenerationTemp)
@@ -155,7 +175,31 @@ public class EnvironmentController : MonoBehaviour
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    //TODO Season Check
+                    if (SeasonEnum == ESeasonEnum.EWinter)
+                    {
+                        maxGenTemp = 8.0f * tempMultiplier;
+                        minGenTemp = -7.0f * tempMultiplier;
+                    }
+                    else if (SeasonEnum == ESeasonEnum.ESpring)
+                    {
+                        maxGenTemp = 12.0f * tempMultiplier;
+                        minGenTemp = 2.0f * tempMultiplier;
+                    }
+                    else if (SeasonEnum == ESeasonEnum.ESummer)
+                    {
+                        maxGenTemp = 20.0f * tempMultiplier;
+                        minGenTemp = 7.0f * tempMultiplier;
+                    }
+                    else if (SeasonEnum == ESeasonEnum.EAutumn)
+                    {
+                        maxGenTemp = 13.0f * tempMultiplier;
+                        minGenTemp = -3.0f * tempMultiplier;
+                    }
+                    else
+                    {
+                        print("Temperature::Check for season failed!");
+                    }
+
                     generatedTemp = Random.Range(minGenTemp, maxGenTemp);
 
                     if((generatedTemp - lastTemp) > 4)
