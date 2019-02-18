9d853ef763ad67a79f9f26552ea9f4e15e502bd6
JensenJ <jensenbradley@icloud.com>
2019-02-18 19:19:28 +0000

    Player Movement added
    Movement of player has been added with a simple jetpack mechanic.
    The prototyping level has been updated to allow for testing on this.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+//Player controller class is mainly for player input, movement is handled by motor.
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float sensitivity = 3.0f;
+    [SerializeField] private float thrust = 1000.0f;
+    //[SerializeField] private float gravityStrength = 10.0f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //Get axis for x and z axis.
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        //get actual relative movement from transform.right/forward
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        //Final movement velocity
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Apply movement on motor
+        motor.Move(velocity);
+
+        //Get axis for rotation
+        float yRot = Input.GetAxisRaw("Mouse X");
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotation = new Vector3(0.0f, yRot, 0.0f) * sensitivity;
+        float cameraRotationX = xRot * sensitivity;
+        //Apply rotation
+        motor.Rotate(rotation);
+        motor.RotateCamera(cameraRotationX);
+
+        if (Input.GetButton("Jump"))
+        {
+            motor.Thrust(thrust);
+        }
+        else
+        {
+            motor.Thrust(0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraRotationX = 0.0f;
+    private float currentCameraRotationX = 0.0f;
+    private float thrust = 0.0f;
+    private float gravityStrength = 9.81f;
+
+    [SerializeField] private float cameraRotationLimit = 85.0f;
+
+    [SerializeField] private Camera cam;
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(1).GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Gets move velocity from player controller input.
+    public void Move(Vector3 m_velocity)
+    {
+        velocity = m_velocity;
+    }
+
+    //Gets rotation from player controller input.
+    public void Rotate(Vector3 m_rotation)
+    {
+        rotation = m_rotation;
+    }
+
+    public void RotateCamera(float m_cameraRotation)
+    {
+        cameraRotationX = m_cameraRotation;
+    }
+
+    public void Thrust(float m_thrust)
+    {
+        thrust = m_thrust;
+    }
+
+    //Performs movement
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + (velocity * Time.fixedDeltaTime));
+        }
+
+        print(thrust);
+        if(thrust != 0)
+        {
+            //Upwards
+            rb.AddForce(new Vector3(0.0f, thrust, 0.0f) * Time.fixedDeltaTime);
+        }
+        else
+        {
+            //Gravity
+            rb.AddForce(new Vector3(0.0f, (-gravityStrength * 200), 0.0f) * Time.fixedDeltaTime);
+        }
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, Mathf.Infinity, layerMask))
+        {
+            //Negative Gravity (Up)
+            rb.AddForce(transform.up * (gravityStrength / (hit.distance / 5f)));
+        }
+    }
+
+    //Performs rotation
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0.0f, 0.0f);
+        }
+    }
+}
