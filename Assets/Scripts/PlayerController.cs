35071b775a2b5fa4a4ad2de84ecf9c7d81fbb9f8
JensenJ <jensenbradley@icloud.com>
2019-02-25 22:12:09 +0000

    Player Controller and Player Motor
    Basic movement controls for when player is not using drone.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2019 JensenJ
+// NAME: PlayerController
+// PURPOSE: Controls for player character (getting input)
+
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Movement Settings")]
+    [SerializeField] private bool bCanMove = true;
+
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float sensitivity = 3.0f;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bCanMove)
+        {
+            Move();
+            Rotate();
+        }
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+    }
+
+    void Rotate()
+    {
+        float yRot = Input.GetAxisRaw("Mouse X");
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotation = new Vector3(0.0f, yRot, 0.0f) * sensitivity;
+        float CameraRotationX = xRot * sensitivity;
+        motor.Rotate(rotation);
+        motor.RotateCamera(CameraRotationX);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2019 JensenJ
+// NAME: PlayerMotor
+// PURPOSE: Apply physics to player character in order to move.
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraRotationX = 0.0f;
+    private float currentCameraRotationX = 0.0f;
+
+    [SerializeField] private float cameraRotationLimit = 85.0f;
+    [SerializeField] private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(1).GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PerformMove();
+        PerformRotate();
+    }
+
+    public void Move(Vector3 m_velocity)
+    {
+        velocity = m_velocity;
+    }
+
+    public void Rotate(Vector3 m_rotation)
+    {
+        rotation = m_rotation;
+    }
+
+    public void RotateCamera(float m_cameraRotation)
+    {
+        cameraRotationX = m_cameraRotation;
+    }
+
+    void PerformMove()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + (velocity * Time.fixedDeltaTime));
+        }
+    }
+
+    void PerformRotate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if(cam != null)
+        {
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0.0f, 0.0f);
+        }
+        else
+        {
+            Debug.LogError("PerformRotation::No camera found");
+        }
+    }
+}
