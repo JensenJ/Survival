4c0a6529705da5c0ddf30083185b66e62d36af19
JensenJ <jensenbradley@icloud.com>
2019-02-26 19:46:59 +0000

    Drone Spawning/Deletion
    Made a method for spawning/deleting the drone. This means the player can deploy the drone from their character.
    
    Bug fixed with player movement:
    When player disables movement while moving, player would continue moving in direction they were going, only input would be disabled with previous method.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [Header("Drone Settings")]
+    [SerializeField] private GameObject droneToSpawn;
+    [SerializeField] private bool bHasDeployedDrone = false;
+    [SerializeField] private float droneHeightSpawnDistance = 0.5f;
+
     //Variables
     [Header("Movement Settings")]
     [SerializeField] private bool bCanMove = true;
-
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float sensitivity = 3.0f;
     [SerializeField] private float jumpForce = 2.0f;
-    PlayerMotor motor;
 
+    private PlayerMotor motor;
+    private GameObject spawnedDrone = null;
     // Setup
     void Start()
     {
@@ -26,37 +31,75 @@ public class PlayerController : MonoBehaviour
     // Update movement
     void Update()
     {
-        //Checks whether player can move
-        if (bCanMove)
-        {
-            Move();
-            Rotate();
-            Jump();
+        Move();
+        Rotate();
+        Jump();
+
+        SpawnDrone();
+    }
+
+    void SpawnDrone()
+    {
+        //Check for drone key
+        if (Input.GetKeyDown(KeyCode.Z))
+        {   
+            //Check whether a drone has already been deployed.
+            if (!bHasDeployedDrone)
+            {
+                //Spawns new drone and disable player controller movement
+                Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y + droneHeightSpawnDistance, transform.position.z);
+                spawnedDrone = Instantiate(droneToSpawn, spawnPos, transform.rotation, transform);
+                bCanMove = false;
+                bHasDeployedDrone = true;
+            }
+            else
+            {
+                //Checks whether drone actually exists
+                if (spawnedDrone != null)
+                {
+                    //Destroys currently spawned drone and enables movement
+                    Destroy(spawnedDrone);
+                    bCanMove = true;
+                }
+                bHasDeployedDrone = false;
+            }
+            
         }
     }
 
     //Movement
     void Move()
     {
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float zMove = Input.GetAxisRaw("Vertical");
+        Vector3 velocity = Vector3.zero;
+        //Checks whether player can move
+        if (bCanMove)
+        {
+            float xMove = Input.GetAxisRaw("Horizontal");
+            float zMove = Input.GetAxisRaw("Vertical");
 
-        Vector3 moveHorizontal = transform.right * xMove;
-        Vector3 moveVertical = transform.forward * zMove;
+            Vector3 moveHorizontal = transform.right * xMove;
+            Vector3 moveVertical = transform.forward * zMove;
 
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+            velocity = (moveHorizontal + moveVertical).normalized * speed;
 
+        }
         motor.Move(velocity);
     }
 
     //Rotation
     void Rotate()
     {
-        float yRot = Input.GetAxisRaw("Mouse X");
-        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 rotation = Vector3.zero;
+        float CameraRotationX = 0.0f;
+        //Checks whether player can move
+        if (bCanMove)
+        {
+            float yRot = Input.GetAxisRaw("Mouse X");
+            float xRot = Input.GetAxisRaw("Mouse Y");
 
-        Vector3 rotation = new Vector3(0.0f, yRot, 0.0f) * sensitivity;
-        float CameraRotationX = xRot * sensitivity;
+            rotation = new Vector3(0.0f, yRot, 0.0f) * sensitivity;
+            CameraRotationX = xRot * sensitivity;
+        }
         motor.Rotate(rotation);
         motor.RotateCamera(CameraRotationX);
     }
@@ -64,14 +107,18 @@ public class PlayerController : MonoBehaviour
     //Jumping
     void Jump()
     {
-        float power;
-        if (Input.GetButtonDown("Jump"))
+        float power = 0.0f;
+        //Checks whether player can move
+        if (bCanMove)
         {
-            power = jumpForce;
-        }
-        else
-        {
-            power = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                power = jumpForce;
+            }
+            else
+            {
+                power = 0.0f;
+            }
         }
         motor.Jump(power);
     }
