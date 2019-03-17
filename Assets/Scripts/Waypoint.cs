8d55dc3c0c412c89040fd761329be0de3bb6c02e
JensenJ <jensenbradley@icloud.com>
2019-03-17 14:16:53 +0000

    Waypoint UI Created
    TextMeshPro has been imported to prevent blurry text.
    Marker prefab created so marker can be placed anywhere, and player will see it.
    Not yet functional.

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2019 JensenJ
+// NAME: FacePlayer
+// PURPOSE: Make game objects face the camera/player
+
+using UnityEngine;
+public class Waypoint : MonoBehaviour
+{
+
+    Transform objectToFace = null;
+    Transform playerPos;
+    PlayerController pc = null;
+    [SerializeField] float distanceFromPlayer;
+
+    //Setup
+    void Start()
+    {
+        objectToFace = transform.root.GetChild(3).transform;
+        playerPos = objectToFace;
+        pc = objectToFace.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pc.bHasDeployedDrone)
+        {
+            objectToFace = playerPos.GetChild(3);
+        }
+        else
+        {
+            objectToFace = playerPos;
+        }
+        
+        if(objectToFace != null)
+        {
+            distanceFromPlayer = Vector3.Distance(objectToFace.position, transform.position);
+            distanceFromPlayer = Mathf.Round(distanceFromPlayer);
+            transform.LookAt(objectToFace);
+            transform.Rotate(0.0f, 180.0f, 0.0f);
+        }
+        else
+        {
+            Debug.LogError("Object to face is null");
+        }
+    }
+}
