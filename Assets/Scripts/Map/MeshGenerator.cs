8aa2ac689bcfd08c573b70fd357cf9388fd10797
JensenJ <jensenbradley@icloud.com>
2019-04-04 21:39:25 +0100

    Basic mesh generation
    Basic mesh generation with simple height and fixed size.

---
+++ Assets/Scripts/Map/MeshGenerator.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) 2019 JensenJ
+// NAME: MeshGenerator
+// PURPOSE: Generates a mesh for the map
+
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    Color[] colours;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    public Gradient gradient;
+    private float minTerrainHeight;
+    private float maxTerrainHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        if (xSize * zSize > 62500)
+        {
+            Debug.LogError("Map size is too large for a single mesh. Cancelling map generation.");
+        }
+        else
+        {
+            CreateShape();
+        }
+
+    }
+
+    private void Update()
+    {                    
+        UpdateMesh();    
+    }                    
+                         
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.1f, z * 0.1f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+
+                if(y > maxTerrainHeight)
+                {
+                    maxTerrainHeight = y;
+                }
+                if(y < minTerrainHeight)
+                {
+                    minTerrainHeight = y;
+                }
+                i++;
+            }
+        }
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+            
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        colours = new Color[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                colours[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colours;
+        mesh.RecalculateNormals();
+    }
+}
