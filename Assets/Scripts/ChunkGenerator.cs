d5bcda8ef02db9bcbc08751184fa2a812b0ffede
JensenJ <jensenbradley@icloud.com>
2019-04-10 23:45:26 +0100

    Removed live generation.
    Removed live generation because of performance reasons and also commented code.

--- Assets/Scripts/ChunkGenerator.cs
+++ Assets/Scripts/ChunkGenerator.cs
@@ -28,14 +28,12 @@ public class ChunkGenerator : MonoBehaviour
 
     //Settings for map generation in editor.
     [SerializeField] bool isTerrainSmooth = false;
-    [SerializeField] public bool liveUpdate = true;
-    [SerializeField] public bool changeSeedOnGen = false;
 
     [SerializeField] float maxHeight = float.MinValue;
     [SerializeField] float minHeight = float.MaxValue;
 
     //Draw new map with seed
-    public void DrawChunk(int m_xSize, int m_zSize, float m_amplitude, float m_frequency, float m_layerHeight, float m_redistribution, int m_seed, int m_xOffset, int m_yOffset, bool m_bIsTerrainSmooth, bool m_bIsLiveUpdate, Material m_mat)
+    public void DrawChunk(int m_xSize, int m_zSize, float m_amplitude, float m_frequency, float m_layerHeight, float m_redistribution, int m_seed, int m_xOffset, int m_yOffset, bool m_bIsTerrainSmooth, Material m_mat)
     {
         //Set variables
         xSize = m_xSize;
@@ -48,7 +46,6 @@ public class ChunkGenerator : MonoBehaviour
         xOffset = m_xOffset;
         yOffset = m_yOffset;
         isTerrainSmooth = m_bIsTerrainSmooth;
-        liveUpdate = m_bIsLiveUpdate;
         GetComponent<MeshRenderer>().sharedMaterial = m_mat;
 
         maxHeight = float.MinValue;
@@ -59,36 +56,15 @@ public class ChunkGenerator : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.name = "Terrain";
 
-        //Check for too many vertices in single mesh.
-        if (xSize * zSize > 62500)
-        {
-            Debug.LogError("Map size is too large for a single mesh. Cancelling map generation.");
-        }
-        else
-        {
-            //Create mesh and update it.
-            CreateMesh();
-            UpdateMesh();
-        }
+        //Create mesh and update it.
+        CreateMesh();
+        UpdateMesh();
 
+        //Collisions
         meshCollider = gameObject.GetComponent<MeshCollider>();
-        //Collisions, do not generate if live update as performance takes a hit, only generates on actual playing and when generate button is pressed
-        if(liveUpdate == false)
-        {
-            meshCollider.enabled = true;
-            meshCollider.sharedMesh = mesh;
-        }
-        else
-        {
-            meshCollider.enabled = false;
-        }
+        meshCollider.enabled = true;
+        meshCollider.sharedMesh = mesh;
     }
-
-    //void Start()
-    //{
-    //    liveUpdate = false;
-    //    DrawChunk(seed);
-    //}
 
     private void Update()
     {
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     //Settings for map generation
-    [SerializeField] [Range(1, 5)] int xChunks = 2;
-    [SerializeField] [Range(1, 5)] int zChunks = 2;
+    [SerializeField] int xChunks = 2;
+    [SerializeField] int zChunks = 2;
 
     [SerializeField] [Range(16, 250)] int xSize = 250;
     [SerializeField] [Range(16, 250)] int zSize = 250;
@@ -17,28 +17,37 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] [Range(0.1f, 3)] float layerHeight = 1.0f;
     [SerializeField] [Range(0.5f, 1.5f)] float redistribution = 1.0f;
     [SerializeField] [Range(-200000, 200000)] public int seed = 0;
-
-    //Settings for map generation in editor.
     [SerializeField] bool isTerrainSmooth = false;
-    [SerializeField] public bool liveUpdate = true;
-    [SerializeField] public bool changeSeedOnGen = false;
+    [SerializeField] Material material = null;
 
-    [SerializeField] Material material;
+    //Function for creating the chunk objects and drawing the map
     public void GenerateMap(int m_seed)
     {
+        //Sets seed
         seed = m_seed;
 
-        for (int z = 0; z < zChunks; z++)
+        //Checks for too many vertices in single mesh.
+        if (xSize * zSize > 62500)
         {
-            for (int x = 0; x < xChunks; x++)
+            Debug.LogError("Map size is too large for a single mesh. Cancelling map generation.");
+        }
+        else
+        {
+            //For each chunk on z axis
+            for (int z = 0; z < zChunks; z++)
             {
-
-                GameObject terrain = new GameObject();
-                terrain.name = "Chunk (" + x + ", " + z + ")";
-                terrain.transform.SetParent(transform);
-                terrain.transform.position = new Vector3(x * xSize, 0, z * zSize);
-                ChunkGenerator cg = terrain.AddComponent<ChunkGenerator>();
-                cg.DrawChunk(xSize, zSize, amplitude, frequency, layerHeight, redistribution, seed, xSize * x, zSize * z, isTerrainSmooth, liveUpdate, material);
+                //For each chunk on x axis
+                for (int x = 0; x < xChunks; x++)
+                {
+                    //Create new terrain object, set name and parent to this. 
+                    //Set position and then call function within Chunk Generator
+                    GameObject terrain = new GameObject();
+                    terrain.name = "Chunk (" + x + ", " + z + ")";
+                    terrain.transform.SetParent(transform);
+                    terrain.transform.position = new Vector3(x * xSize, 0, z * zSize);
+                    ChunkGenerator cg = terrain.AddComponent<ChunkGenerator>();
+                    cg.DrawChunk(xSize, zSize, amplitude, frequency, layerHeight, redistribution, seed, xSize * x, zSize * z, isTerrainSmooth, material);
+                }
             }
         }
     }
@@ -46,13 +55,6 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        liveUpdate = false;
         GenerateMap(seed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
