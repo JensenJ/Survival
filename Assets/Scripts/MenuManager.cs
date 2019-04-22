8dde6ca8670ec94dbe11fe66fe2ad5e2178766cc
JensenJ <jensenbradley@icloud.com>
2019-04-22 18:09:59 +0100

    Main Menu Button Functionality
    The Main Menu is now functional with singleplayer and quitting the game buttons.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Singleplayer()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Multiplayer()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
